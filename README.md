5db1146be032ef9bd2329f1878b6e4111a309122
Amie Davis <amiedav@gmail.com>
2018-06-27 23:35:40 +1000

    Initial commit. Basic page with a button and events displaying.
    Note that I have left in some of the create-react-app code so I can learn from it as I go.

---
+++ README.md
@@ -0,0 +1,5 @@
+Often when people are depressed, they find it difficult to work up the motivation to do activities that they used to find enjoyable or satisfyiing. Sometimes, people who are depressed can't even remember what they used to enjoy.
+
+The purpose of this project is to develop a website that offers up suggestions for activities people can try when they are struggling to think of what to do.
+
+Initially, this website will be very simple as I am using it as a tool for learning. However, ideally I would like to extend the idea to allow people to track activities that they enjoy, and furthermore include a rating system that allows people to keep track of their mood before and after undertaking a certain activity.
---
+++ src/index.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import activities from './activities.json';
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'We have a weiner! Winner is: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class ActivityGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activityNumber: null
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="main-button-container">
+          <button className="main-button" onClick={() => this.setActivityNumber()}>
+            WTF Should I do?
+          </button>
+        </div>
+        <div className="activity-container">
+          {this.renderActivity()}
+        </div>
+      </div>
+    );
+  }
+
+  setActivityNumber() {
+    let randomNumber = Math.floor(Math.random()*activities.length);
+    this.setState({
+      activityNumber: randomNumber
+    });
+  }
+
+  renderActivity() {
+    return (
+      <Activity
+        value={this.state.activityNumber}
+      />
+    )
+  }
+}
+
+function Activity(props){
+  return(
+    <div className="activity">
+      {props.value != null ? activities[props.value].activity : ""}
+    </div>
+  );
+}
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+ReactDOM.render(
+  <ActivityGenerator />,
+  document.getElementById('root')
+);
